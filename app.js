9e895068eeb35e4e8d58650bd9e10dd8fd683358
yusufardianto <muhammadyusufardianto@gmail.com>
2023-03-14 22:42:11 +0700

    app.js

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+  res.send(
+    "<h1>Demo Express App</h1><h4>Message: Success</h4> <p>Version 1.0</p>"
+  );
+});
+
+app.get("/products", (req, res) => {
+  res.send([
+    {
+      productId: "pd001",
+      price: 100,
+    },
+    {
+      productId: "pd002",
+      price: 150,
+    },
+    {
+      productId: "pd003",
+      price: 100,
+    },
+  ]);
+});

3ff15b6f0c255cada9be54c5c644f1848e2f3ad0
yusufardianto <muhammadyusufardianto@gmail.com>
2023-03-20 21:34:26 +0700

    script

---
+++ scripts/application_start.sh
@@ -0,0 +1,16 @@
+# menjalankan perintah sebagai admin(sementara) untuk mengelola directory
+sudo chmod -R 777 /home/ec2-user/node-aws
+
+# pindah ke directory
+cd /home/ec-2-user/node-aws
+
+# add npm and node to path
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"
+
+# install node modul
+npm install
+
+# start node app ind the background
+node index.js > app.out.log 2> app.err.log < /dev/null &
---
+++ scripts/before_install.sh
@@ -0,0 +1,16 @@
+# #download node and npm
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
+
+# . ~/.nvm/nvm.sh
+# nvm install node
+
+# buat directory jika directory tidak ada
+DIR="/home/ec2-user/node-aws"
+if [-d "$DIR"]; then
+    echo "${DIR} exists"
+else
+    echo "Creating ${DIR} directory"
+    mkdir ${DIR}
+fi
+
+
